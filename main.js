e629fcc780a3bc927b6ea48fb4155ab0f8cdc1c3
giuseppedelcorso <Giuseppedecoboolean@gmail.com>
2023-11-14 16:14:37 +0100

    sv

--- main.js
+++ main.js
@@ -11,15 +11,15 @@ const posts = [
         "created": "2021-06-25"
     },
     {
-        "id": 2,
-        "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
-        "media": "https://unsplash.it/600/400?image=112",
-        "author": {
-            "name": "Sofia Perlari",
-            "image": "https://unsplash.it/300/300?image=10"
+        id: 2,
+        content: "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
+        media: "https://unsplash.it/600/400?image=112",
+        author: {
+            name: "Sofia Perlari",
+            image: "https://unsplash.it/300/300?image=10"
         },
-        "likes": 120,
-        "created": "2021-09-03"
+        likes: 120,
+        created: "2021-09-03"
     },
     {
         "id": 3,
@@ -56,27 +56,6 @@ const posts = [
     }
 ];
  let container = document.getElementById("container")
- container.innerHTML = '<div class="post"><div class="post__header"><div class="post-meta"><div class="post-meta__icon"><img class="profile-pic" src="https://unsplash.it/300/300?image=15" alt="Phil Mangione"></div><div class="post-meta__data">
-             <div class="post-meta__author">Phil Mangione</div>
-             <div class="post-meta__time">4 mesi fa</div>
-         </div>                    
-     </div>
- </div>
- <div class="post__text">Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.</div>
- <div class="post__image">
-     <img src="https://unsplash.it/600/300?image=171" alt="">
- </div>
- <div class="post__footer">
-     <div class="likes js-likes">
-         <div class="likes__cta">
-             <a class="like-button  js-like-button" href="#" data-postid="1">
-                 <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
-                 <span class="like-button__label">Mi Piace</span>
-             </a>
-         </div>
-         <div class="likes__counter">
-             Piace a <b id="like-counter-1" class="js-likes-counter">80</b> persone
-         </div>
-     </div> 
- </div>            
-</div>
+ let container2 = document.getElementById("containerDue")
+ container.innerHTML = '<div class="post"><div class="post__header"><div class="post-meta"><div class="post-meta__icon"><img class="profile-pic" src="https://unsplash.it/300/300?image=15" alt="Phil Mangione"></div><div class="post-meta__data"><div class="post-meta__author">Phil Mangione</div><div class="post-meta__time">4 mesi fa</div></div></div></div><div class="post__text">Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.</div><div class="post__image"><img src="https://unsplash.it/600/300?image=171" alt=""></div><div class="post__footer"><div class="likes js-likes"><div class="likes__cta"><a class="like-button  js-like-button" href="#" data-postid="1"><i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i><span class="like-button__label">Mi Piace</span></a></div><div class="likes__counter"> Piace a <b id="like-counter-1" class="js-likes-counter">80</b> persone</div></div></div></div>'
+container2.innerHTML = '<div class="post"><div class="post__header"><div class="post-meta"><div class="post-meta__icon"><img class="profile-pic" src='+posts[1].media+'alt="Phil Mangione"></div><div class="post-meta__data"><div class="post-meta__author">'+posts[1].author.name+'</div><div class="post-meta__time">'+posts[1].created+'</div></div></div></div><div class="post__text">Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.</div><div class="post__image"><img src='+posts[1].author.image+' alt=""></div><div class="post__footer"><div class="likes js-likes"><div class="likes__cta"><a class="like-button  js-like-button" href="#" data-postid="1"><i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i><span class="like-button__label">Mi Piace</span></a></div><div class="likes__counter"> Piace a <b id="like-counter-1" class="js-likes-counter">'+posts[1].likes+'</b> persone</div></div></div></div>'
