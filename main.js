00bad4bc61f41ebdb31c276be85009531970a35f
giuseppedelcorso <Giuseppedecoboolean@gmail.com>
2023-11-14 16:46:24 +0100

    fine

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
 const posts = [
     {
-        "id": 1,
-        "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
-        "media": "https://unsplash.it/600/300?image=171",
-        "author": {
-            "name": "Phil Mangione",
-            "image": "https://unsplash.it/300/300?image=15"
+        id: 1,
+        content: "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
+        media: "https://unsplash.it/600/300?image=171",
+        author: {
+            name: "Phil Mangione",
+            image: "https://picsum.photos/seed/picsum/200/300"
         },
-        "likes": 80,
-        "created": "2021-06-25"
+        likes: 80,
+        created: "2021-06-25"
     },
     {
         id: 2,
@@ -22,40 +22,44 @@ const posts = [
         created: "2021-09-03"
     },
     {
-        "id": 3,
-        "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
-        "media": "https://unsplash.it/600/400?image=234",
-        "author": {
-            "name": "Chiara Passaro",
-            "image": "https://unsplash.it/300/300?image=20"
+        id: 3,
+        content: "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
+        media: "https://unsplash.it/600/400?image=234",
+        author: {
+            name: "Chiara Passaro",
+            image: "https://unsplash.it/300/300?image=20"
         },
-        "likes": 78,
-        "created": "2021-05-15"
+        likes: 78,
+        created: "2021-05-15"
     },
     {
-        "id": 4,
-        "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
-        "media": "https://unsplash.it/600/400?image=24",
-        "author": {
-            "name": "Luca Formicola",
-            "image": null
+        id: 4,
+        content: "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
+        media: "https://unsplash.it/600/400?image=24",
+        author: {
+            name: "Luca Formicola",
+            image: null
         },
-        "likes": 56,
-        "created": "2021-04-03"
+        likes: 56,
+        created: "2021-04-03"
     },
     {
-        "id": 5,
-        "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
-        "media": "https://unsplash.it/600/400?image=534",
-        "author": {
-            "name": "Alessandro Sainato",
-            "image": "https://unsplash.it/300/300?image=29"
+        id: 5,
+        content: "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
+        media: "https://unsplash.it/600/400?image=534",
+        author: {
+            name: "Alessandro Sainato",
+            image: "https://unsplash.it/300/300?image=29"
         },
-        "likes": 95,
-        "created": "2021-03-05"
+        likes: 95,
+        created: "2021-03-05"
     }
 ];
  let container = document.getElementById("container")
  let container2 = document.getElementById("containerDue")
- container.innerHTML = '<div class="post"><div class="post__header"><div class="post-meta"><div class="post-meta__icon"><img class="profile-pic" src="https://unsplash.it/300/300?image=15" alt="Phil Mangione"></div><div class="post-meta__data"><div class="post-meta__author">Phil Mangione</div><div class="post-meta__time">4 mesi fa</div></div></div></div><div class="post__text">Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.</div><div class="post__image"><img src="https://unsplash.it/600/300?image=171" alt=""></div><div class="post__footer"><div class="likes js-likes"><div class="likes__cta"><a class="like-button  js-like-button" href="#" data-postid="1"><i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i><span class="like-button__label">Mi Piace</span></a></div><div class="likes__counter"> Piace a <b id="like-counter-1" class="js-likes-counter">80</b> persone</div></div></div></div>'
-container2.innerHTML = '<div class="post"><div class="post__header"><div class="post-meta"><div class="post-meta__icon"><img class="profile-pic" src='+posts[1].media+'alt="Phil Mangione"></div><div class="post-meta__data"><div class="post-meta__author">'+posts[1].author.name+'</div><div class="post-meta__time">'+posts[1].created+'</div></div></div></div><div class="post__text">Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.</div><div class="post__image"><img src='+posts[1].author.image+' alt=""></div><div class="post__footer"><div class="likes js-likes"><div class="likes__cta"><a class="like-button  js-like-button" href="#" data-postid="1"><i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i><span class="like-button__label">Mi Piace</span></a></div><div class="likes__counter"> Piace a <b id="like-counter-1" class="js-likes-counter">'+posts[1].likes+'</b> persone</div></div></div></div>'
+let i = 0
+ for (let i = 0; i <= posts.length -1; i++) {
+    
+     container.innerHTML += '<div class="post"><div class="post__header"><div class="post-meta"><div class="post-meta__icon"><img class="profile-pic" src='+posts[i].author.image+'alt="Phil Mangione"></div><div class="post-meta__data"><div class="post-meta__author">'+posts[i].author.name+'</div><div class="post-meta__time">'+posts[i].created+'</div></div></div></div><div class="post__text">Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.</div><div class="post__image"><img src='+posts[i].media+' alt=""></div><div class="post__footer"><div class="likes js-likes"><div class="likes__cta"><a class="like-button  js-like-button" href="#" data-postid="1"><i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i><span class="like-button__label">Mi Piace</span></a></div><div class="likes__counter"> Piace a <b id="like-counter-1" class="js-likes-counter">'+posts[i].likes+'</b> persone</div></div></div></div>'
+    
+ }
