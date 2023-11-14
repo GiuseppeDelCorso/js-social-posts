c827065e145fcf0c18e312dde1fd59ccc4d612ae
giuseppedelcorso <Giuseppedecoboolean@gmail.com>
2023-11-14 15:36:44 +0100

    svc

--- main.js
+++ main.js
@@ -56,13 +56,7 @@ const posts = [
     }
 ];
  let container = document.getElementById("container")
- container.innerHTML = "<div class="post">"+
- "<div class="post__header">"+
-     "<div class="post-meta">"+                   
-         "<div class="post-meta__icon">"+
-             "<img class="profile-pic" src="https://unsplash.it/300/300?image=15" alt="Phil Mangione">"+                    
-         </div>
-         <div class="post-meta__data">
+ container.innerHTML = '<div class="post"><div class="post__header"><div class="post-meta"><div class="post-meta__icon"><img class="profile-pic" src="https://unsplash.it/300/300?image=15" alt="Phil Mangione"></div><div class="post-meta__data">
              <div class="post-meta__author">Phil Mangione</div>
              <div class="post-meta__time">4 mesi fa</div>
          </div>                    
